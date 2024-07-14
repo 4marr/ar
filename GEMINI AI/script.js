85a74ca258a3dd08efda012971042b0a63da67f2
Ammar <144656550+4marr@users.noreply.github.com>
2024-07-14 16:06:25 +0700

    Add files via upload

--- GEMINI AI/script.js
+++ GEMINI AI/script.js
@@ -44,7 +44,7 @@ const createChatLi = (message, className) => {
 let generateResponse = (incomingChatLi) => {
     const prompt = document.getElementById('message').value;
     var percakapanTerakhir;
-    let apiUrl = `https://itzpire.com/ai/gemini-ai?q=${encodeURIComponent(prompt)}`;
+    let apiUrl = `https://api.nyxs.pw/ai/gemini?text=${encodeURIComponent(prompt)}`;
     let hasil = incomingChatLi.querySelector("p")
     
     fetch(apiUrl)
